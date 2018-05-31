e0ee4c3d79d82563be4ed5afe6ae8b09d6bec2a0
Richard L. Jackson <denezt@users.noreply.github.com>
2018-05-31 10:37:50 +0200

    Updating, dependencies script
    Updating, dependencies script to add Ubuntu as a possible distribution.

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-# Install Prereqs for scripts.
-zypper in rpm-build || dnf in rpm-build
-zypper in pwgen || dnf in pwgen
+_distro="$1"
 
+# Install Prereqs for scripts.
+red_hat(){
+  zypper in rpm-build || dnf in rpm-build
+  zypper in pwgen || dnf in pwgen
+}
+
+ubuntu(){
+  printf "Checking for required linked resources...\n"
+  test -e "/lib/libc.so.6" || ln -s /lib/i386-linux-gnu/libc.so.6 /lib/
+  printf "Done!\n"
+  apt-get -y install alien
+}
+
+help_menu(){
+  printf "Install Build Deps"
+  printf "Redhat distro\t[ -redhat, -fedora ]\n"
+  printf "Ubuntu distro\t[ -ubuntu ]\n"
+}
+
+case ${_distro} in
+  -redhat|-fedora) red_hat;;
+  -ubuntu) ubuntu;;
+esac
