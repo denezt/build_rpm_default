063e247366e0ab3b6f441323d1cc7cdf00f9244b
Richard L. Jackson <denezt@users.noreply.github.com>
2018-10-04 12:13:28 +0200

    Create Jenkinsfile.groovy
    Placeholder taken from: https://github.com/spineo/jenkinsfile/blob/master/Jenkinsfile

---
+++ Jenksinfile.groovy
@@ -0,0 +1,37 @@
+node {
+    try {
+        stage('Checkout') {
+              // To be added to shared libraries
+              //
+              checkout scm
+              //checkout changelog: false, poll: false, \
+              //scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: \
+              //   [[$class: 'ScmName', name: 'gradle']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Database', \
+              //   url: 'https://github.com/spineo/groovy-gradle-project.git']]]
+        }
+        
+        stage('Clean') {
+            sh "./gradlew clean"
+        }
+        
+        stage('Build') {
+           echo 'Build'
+        }
+
+        stage('Testing') {
+            echo 'Testing'
+        }
+
+        stage('Staging') {
+            echo 'Deploy Stage'
+        }
+        stage('Deploy') {
+            echo 'Deploy Stage'
+        }
+  } catch (e) {
+    currentBuild.result = "FAILED"
+    throw e
+  } finally {
+    echo 'Success'
+  }
+}
