e362afba738cdcea7d1c4f2e2580e075fe297a58
Richard Jackson <denezt@yahoo.com>
2024-10-06 00:20:21 +0200

    Adding version information to displaymsg program.

--- displaymsg-1.0/main.c
+++ displaymsg-1.0/main.c
@@ -4,8 +4,12 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <math.h>
 
 int main(int argc, char *argv[]){
 	char *s = *argv++;
 	printf("Program installed %s\n", s);
+	printf("version 1.0.1\n");
+	return 0;
 }
